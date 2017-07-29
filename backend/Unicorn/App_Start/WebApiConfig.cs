af639a612217c1af9a092e210acaca4333288484
definetux <goncharuk.m.n@gmail.com>
2017-07-29 19:33:16 +0300

    Changed default web api route template

--- backend/Unicorn/App_Start/WebApiConfig.cs
+++ backend/Unicorn/App_Start/WebApiConfig.cs
@@ -18,7 +18,7 @@ namespace Unicorn
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
