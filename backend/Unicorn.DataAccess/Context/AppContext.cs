156f368b2e1110a6c45ba7ee456ecff4292df981
tyurtyubek <tyurtyubek@outlook.com>
2017-08-07 21:10:42 +0300

    Add EF and Context

---
+++ backend/Unicorn.DataAccess/Context/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace Unicorn.DataAccess.Context
+{
+    class AppContext: DbContext
+    {
+
+        public AppContext(string connectionString)
+        : base(connectionString)
+        {
+
+        }
+    }
+
+}
