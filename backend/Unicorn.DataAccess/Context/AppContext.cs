63927ec12366c016177e6b6982976e437cd0d6d7
Taras Parfeniuk <taras.parfeniuk@gmail.com>
2017-08-08 12:51:03 +0300

    Added dbsets to context

--- backend/Unicorn.DataAccess/Context/AppContext.cs
+++ backend/Unicorn.DataAccess/Context/AppContext.cs
@@ -1,9 +1,26 @@
 ﻿using System.Data.Entity;
 
+using Unicorn.DataAccess.Entities;
+
 namespace Unicorn.DataAccess.Context
 {
     class AppContext: DbContext
     {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<History> History { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Subcategory> Subcategories { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Work> Works { get; set; }
+
         public AppContext(string connectionString) : base(connectionString)
         {
 
