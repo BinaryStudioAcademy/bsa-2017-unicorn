6ebd1a4d5621178fc99ad1abe0dae1b77568acb1
Oleg Kopot <o.d.kopot@yandex.ru>
2017-08-09 16:05:38 +0300

    Add company profile page (not complete)

---
+++ frontend/src/app/company/company-details/reviews/reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'company-reviews',
+  templateUrl: './reviews.component.html',
+  styleUrls: ['./reviews.component.css']
+})
+export class ReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- frontend/src/app/company/company.module.ts
+++ frontend/src/app/company/company.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import {SuiModule} from 'ng2-semantic-ui';
 
 import { CompanyComponent } from './company-component/company.component';
 import { CompanyDetailsComponent } from './company-details/company-details.component';
 import { CompanyRoutingModule } from './company-routing.module';
+import { GeneralInformationComponent } from './company-details/general-information/general-information.component';
+import { ReviewsComponent } from './company-details/reviews/reviews.component';
+import { VendorsComponent } from './company-details/vendors/vendors.component';
+import { ContactsComponent } from './company-details/contacts/contacts.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    CompanyRoutingModule
+    CompanyRoutingModule,
+    FormsModule,
+    BrowserModule,
+    SuiModule
   ],
   declarations: [
     CompanyComponent,
-    CompanyDetailsComponent
+    CompanyDetailsComponent,
+    GeneralInformationComponent,
+    ReviewsComponent,
+    VendorsComponent,
+    ContactsComponent
   ]
 })
 export class CompanyModule { }
