7c300812a1b529752528ef406323b87817b2ca5b
Ihor Horash <gorashigor@gmail.com>
2017-08-09 21:48:33 +0300

    Landing searchbar was completed

--- frontend/src/app/index/index.module.ts
+++ frontend/src/app/index/index.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SuiModule } from 'ng2-semantic-ui';
 
 import { IndexComponent } from './index-component/index.component';
 import { IndexRoutingModule } from './index-routing.module';
@@ -7,7 +9,9 @@ import { IndexRoutingModule } from './index-routing.module';
 @NgModule({
   imports: [
     CommonModule,
-    IndexRoutingModule
+    IndexRoutingModule,
+    FormsModule,
+    SuiModule
   ],
   declarations: [
     IndexComponent
