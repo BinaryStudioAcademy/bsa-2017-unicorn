ce54e77b1a20c42b6f6d12db1894a14d994f86e8
BogdanaOst <bogdanochkaost@gmail.com>
2017-08-10 19:01:00 +0300

    ui upds

--- frontend/src/app/user/user-history/user-history.component.ts
+++ frontend/src/app/user/user-history/user-history.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { User } from '../../models/user';
 @Component({
   selector: 'app-user-history',
   templateUrl: './user-history.component.html',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class UserHistoryComponent implements OnInit {
 
   constructor() { }
-
+  @Input() user: User;
   ngOnInit() {
   }
 
