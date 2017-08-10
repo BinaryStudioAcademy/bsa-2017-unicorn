050cc8788878a4a0cae40b664fe2df01230ba747
Ihor Horash <gorashigor@gmail.com>
2017-08-10 10:22:49 +0300

    Added suitabs

--- frontend/src/app/index/index-component/popular-tasks/popular-tasks.component.ts
+++ frontend/src/app/index/index-component/popular-tasks/popular-tasks.component.ts
@@ -7,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popular-tasks.component.css']
 })
 export class PopularTasksComponent implements OnInit {
+  // tabs: {id: number, name: string}[];
+  tabs: Tab[];
 
   constructor() { }
 
   ngOnInit() {
+    this.createTabs();
   }
 
+  createTabs() {
+    this.tabs = [{
+      id: 1,
+      name: 'Popular'
+    }, {
+      id: 2,
+      name: 'Cleaning',
+    }, {
+      id: 3,
+      name: 'Building',
+    }, {
+      id: 4,
+      name: 'Relocation',
+    }, {
+      id: 5,
+      name: 'Сourier',
+    }];
+  }
+
+}
+
+export class Tab {
+  id: number;
+  name: string;
 }
