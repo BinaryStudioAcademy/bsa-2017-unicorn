fc8f155c44a5809d0a5c75d4a5611ca63db8afff
Ihor Horash <gorashigor@gmail.com>
2017-08-11 13:13:02 +0300

    Added unsubscription for router event of landing

--- frontend/src/app/shell/shell.component.ts
+++ frontend/src/app/shell/shell.component.ts
@@ -1,29 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
+
+import { ISubscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.css']
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent implements OnInit, OnDestroy {
   location: string;
+  private subscription: ISubscription;
 
   constructor(private router: Router) {
   }
 
   ngOnInit() {
     this.getCurrentUrl();
-    console.log(this.location);
   }
 
   getCurrentUrl() {
-    this.router.events
+    this.subscription = this.router.events
     .subscribe((event) => {
       if (event instanceof NavigationStart) {
         this.location = event.url;
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
