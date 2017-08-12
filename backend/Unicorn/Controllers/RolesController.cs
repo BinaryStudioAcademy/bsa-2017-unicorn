5325d6a7e259b812951fc49afdc3f610166e86ce
Bogdan Balanyk <balanykb@gmail.com>
2017-08-12 22:52:45 +0300

    Add basic functionality for RolesController

---
+++ backend/Unicorn/Controllers/RolesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Unicorn.Core.DTOs;
+using Unicorn.Core.Interfaces;
+
+namespace Unicorn.Controllers
+{
+    public class RolesController : ApiController
+    {
+        private readonly IRoleService _roleService;
+
+        public RolesController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        // GET: roles/1
+        [ResponseType(typeof(RoleDTO))]
+        [Route("roles/{uid}")]
+        public async Task<IHttpActionResult> GetRole(long uid)
+        {
+            var result = await _roleService.GetByUserId(uid);
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
