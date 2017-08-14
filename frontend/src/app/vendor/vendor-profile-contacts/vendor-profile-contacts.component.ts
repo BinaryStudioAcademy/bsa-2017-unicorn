b354ddbfaf79b8ed43b358978317356b0ab06e9e
Taras Parfeniuk <taras.parfeniuk@gmail.com>
2017-08-14 03:12:11 +0300

    Insert map into vendor contacts

--- frontend/src/app/vendor/vendor-profile-contacts/vendor-profile-contacts.component.ts
+++ frontend/src/app/vendor/vendor-profile-contacts/vendor-profile-contacts.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewChecked, ViewChild } from '@angular/core';
+
+import { AgmMap, AgmMarker } from "@agm/core";
 
 import { Vendor } from '../../models/vendor.model';
 
 @Component({
   selector: 'app-vendor-profile-contacts',
   templateUrl: './vendor-profile-contacts.component.html',
-  styleUrls: ['./vendor-profile-contacts.component.sass']
+  styleUrls: ['./vendor-profile-contacts.component.sass'],
 })
-export class VendorProfileContactsComponent implements OnInit {
-  @Input() vendorId: number;
-  
+export class VendorProfileContactsComponent implements OnInit, AfterViewChecked {
+  @Input() private vendorId: number;
+  @ViewChild(AgmMap) private map: AgmMap;
+
+  lat: number = 49.85711;
+  lng: number = 24.01980; 
+  markerTitle: string = 'Title';
+
   constructor() { }
+
+  ngAfterViewChecked() {
+    this.map.triggerResize();
+  }
 
   ngOnInit() {
   }
-
 }
