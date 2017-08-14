c7358be30531de3abc043f01f45b8ceb00195f80
BogdanaOst <bogdanochkaost@gmail.com>
2017-08-14 16:12:15 +0300

    UI upds

--- frontend/src/app/user/user-profile/user-profile.component.ts
+++ frontend/src/app/user/user-profile/user-profile.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+﻿import { Component, OnInit, Input, ViewChild, AfterViewChecked } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { User } from '../../models/user';
+import { AgmMap } from "@agm/core";
+export interface IContext {
+    data: string;
+}
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.sass']
+    selector: 'app-user-profile',
+    templateUrl: './user-profile.component.html',
+    styleUrls: ['./user-profile.component.sass']
 })
+
+
 export class UserProfileComponent implements OnInit {
 
-  
-  @Input() user: User;
-  constructor() { }
+    @Input() user: User;
+    @ViewChild(AgmMap) private map: any;
+    lat: number = 49.85711;
+    lng: number = 24.01980;
 
-  ngOnInit() {
-  }
-
+    // constructor() { }
+    //  mapClicked($event: MouseEvent) {
+    //  this.lat = $event.coords.lat;
+    //  this.lng = $event.coords.lng;
+    //}
+    ngOnInit() {
+    }
 }
--- frontend/src/app/user/user.module.ts
+++ frontend/src/app/user/user.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserComponent } from './user-component/user.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 
 import { UserRoutingModule } from './user-routing.module';
-import {SuiModule} from 'ng2-semantic-ui';
+import { SuiModule } from 'ng2-semantic-ui';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserHistoryComponent } from './user-history/user-history.component';
 import { UserTasksComponent } from './user-tasks/user-tasks.component';
@@ -13,24 +13,29 @@ import { UserMessagesComponent } from './user-messages/user-messages.component';
 
 import { UserService } from '../services/user.service';
 import { DataService } from "../services/data.service";
+import { AgmCoreModule } from "@agm/core";
+import { environment } from "../../environments/environment";
 
 @NgModule({
-  imports: [
-    SuiModule,
-    CommonModule,
-    UserRoutingModule
-  ],
-  declarations: [
-    UserComponent,
-    UserDetailsComponent,
-    UserProfileComponent,
-    UserHistoryComponent,
-    UserTasksComponent,
-    UserMessagesComponent
-  ],
-  providers: [
-    DataService,
-    UserService
-  ]
+    imports: [
+        AgmCoreModule.forRoot({
+            apiKey: environment.googleMapsKey
+        }),
+        SuiModule,
+        CommonModule,
+        UserRoutingModule
+    ],
+    declarations: [
+        UserComponent,
+        UserDetailsComponent,
+        UserProfileComponent,
+        UserHistoryComponent,
+        UserTasksComponent,
+        UserMessagesComponent
+    ],
+    providers: [
+        DataService,
+        UserService
+    ]
 })
 export class UserModule { }
