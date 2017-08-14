04b8e2f6f1948ecc0a698558957adb24e05c4b44
BogdanaOst <bogdanochkaost@gmail.com>
2017-08-15 00:38:57 +0300

    build fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,7 +24,6 @@ import { UserModule } from './user/user.module';
 import { VendorModule } from './vendor/vendor.module';
 import { BookModule } from './book/book.module';
 import { IndexModule } from './index/index.module';
-import { ImageUploadModule } from "angular2-image-upload";
 
 
 
@@ -47,7 +46,6 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ImageUploadModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase, 'unicorn-angular'),
     AngularFireAuthModule,
     CategoryModule,
