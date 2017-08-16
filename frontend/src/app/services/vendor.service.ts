c77562b40ade7ffaf762467dea4e041e4ec6afee
Taras Parfeniuk <taras.parfeniuk@gmail.com>
2017-08-16 14:47:24 +0300

    Small services fix

--- frontend/src/app/services/vendor.service.ts
+++ frontend/src/app/services/vendor.service.ts
@@ -20,14 +20,14 @@ export class VendorService {
   }
 
   getVendor(id: number) : Promise<Vendor> {
-    return this.dataService.getRequest<any>(`${this.resourceUrl}/${id}`);
+    return this.dataService.getRequest<Vendor>(`${this.resourceUrl}/${id}`);
   }
 
   getRating(id: number): Promise<Rating> {
-    return this.dataService.getRequest<any>(`${this.resourceUrl}/${id}/rating`);
+    return this.dataService.getRequest<Rating>(`${this.resourceUrl}/${id}/rating`);
   }
 
   getVendorPorfolio(vendorId: number): Promise<PortfolioItem[]> {
-    return this.dataService.getRequest<any>(`${this.resourceUrl}/${vendorId}/portfolio`);
+    return this.dataService.getRequest<PortfolioItem[]>(`${this.resourceUrl}/${vendorId}/portfolio`);
   }
 }
