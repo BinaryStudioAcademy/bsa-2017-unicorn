6dacbd063b208fefed505e15c9e0c5b9e2f1d621
Ihor Horash <gorashigor@gmail.com>
2017-08-17 00:10:20 +0300

    Added companies list to search page

--- frontend/src/app/search/search-component/search.component.ts
+++ frontend/src/app/search/search-component/search.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Company } from '../../models/company.model';
+import { Review } from '../../models/review.model';
+import { CompanyService } from '../../services/company.service';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  companies: Company[] = [];
+  /* pagination */
+  pageSize = 12;
+  maxSize = 3;
+  hasEllipses = true;
+  selectedPage = 1;
 
-  constructor() { }
+  constructor(
+    private companyService: CompanyService
+  ) { }
 
   ngOnInit() {
+    this.createMockCompanies(20);
+  }
+
+  createMockCompanies(num: number) {
+    let i: number;
+    for (i = num; i >= 1; i--) {
+      this.companies.push(this.companyService.getCompany());
+    }
   }
 
 }
--- frontend/src/app/search/search.module.ts
+++ frontend/src/app/search/search.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SuiModule } from 'ng2-semantic-ui';
 import { SearchComponent } from './search-component/search.component';
 
 import { SearchRoutingModule } from './search-routing.module';
 
+import { Company } from '../models/company.model';
+import { Review } from '../models/review.model';
+import { Vendor } from '../models/vendor';
+import { environment } from '../../environments/environment';
+import { CompanyService } from '../services/company.service';
+import { NguiMapModule } from '@ngui/map';
+import { MapComponent } from '../map/map.component';
+
 @NgModule({
   imports: [
     CommonModule,
-    SearchRoutingModule
+    SearchRoutingModule,
+    FormsModule,
+    SuiModule
   ],
-  declarations: [SearchComponent]
+  declarations: [SearchComponent],
+  providers: [
+    CompanyService
+  ]
 })
 export class SearchModule { }
