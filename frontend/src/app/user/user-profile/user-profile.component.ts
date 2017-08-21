4f344fe24180db7ac26da00a0ea4a32490f846c3
Yuriy Kuchevskiy <yurakuchevskiy@mail.ru>
2017-08-21 19:44:09 +0300

    Added validation for vendor and company

--- frontend/src/app/user/user-profile/user-profile.component.ts
+++ frontend/src/app/user/user-profile/user-profile.component.ts
@@ -21,7 +21,7 @@ export class UserProfileComponent implements OnInit {
     @ViewChild(AgmMap) private map: any;
       location: Location;
 
-    @ViewChild('userForm') userForm: NgForm;
+    @ViewChild('userForm') public userForm: NgForm;
     lat: number = 48.464921;
     lng: number = 35.045798;
     birthday: Date;
