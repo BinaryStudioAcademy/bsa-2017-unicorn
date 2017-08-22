f1332afec96418ba0c009ef810209cef6ac30b1c
Nikita <dev.potapy4@hotmail.com>
2017-08-22 14:48:20 +0300

    Fixed roles | save profileid in JWT

--- backend/Unicorn.Core/Services/CompanyService.cs
+++ backend/Unicorn.Core/Services/CompanyService.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Threading.Tasks;
 using Unicorn.Core.Interfaces;
-using Unicorn.Core.Services.Helpers;
 using Unicorn.DataAccess.Entities;
+using Unicorn.DataAccess.Entities.Enum;
 using Unicorn.DataAccess.Interfaces;
-using Unicorn.Shared.DTOs;
-using Unicorn.Shared.DTOs.Company;
-using Unicorn.Shared.DTOs.Contact;
 using Unicorn.Shared.DTOs.Register;
-using Unicorn.Shared.DTOs.Vendor;
 
 namespace Unicorn.Core.Services
 {
@@ -28,7 +21,7 @@ namespace Unicorn.Core.Services
         public async Task Create(CompanyRegisterDTO companyDto)
         {
             var account = new Account();
-            var role = await _unitOfWork.RoleRepository.GetByIdAsync((long)AccountRoles.Company);
+            var role = await _unitOfWork.RoleRepository.GetByIdAsync((long)RoleType.Company);
             var socialAccounts = new List<SocialAccount>();
             var socialAccount = new SocialAccount();
             var company = new Company();
