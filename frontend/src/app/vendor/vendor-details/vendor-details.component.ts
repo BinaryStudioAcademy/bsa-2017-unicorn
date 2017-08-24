17c5491711070c55edc10ae05db502e05b8963c8
Nikita <dev.potapy4@hotmail.com>
2017-08-24 10:19:52 +0300

    TokenHelperService in vendor-details

--- frontend/src/app/vendor/vendor-details/vendor-details.component.ts
+++ frontend/src/app/vendor/vendor-details/vendor-details.component.ts
@@ -1,39 +1,39 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
-import { JwtHelper } from '../../helpers/jwthelper';
 import 'rxjs/add/operator/switchMap';
 
-
 import { Vendor } from '../../models/vendor.model';
+import { VendorService } from '../../services/vendor.service';
+import { ModalService } from '../../services/modal/modal.service';
+import { TokenHelperService } from '../../services/helper/tokenhelper.service';
 
-import { VendorService } from "../../services/vendor.service";
-import { ModalService } from "../../services/modal/modal.service";
+import {
+  SuiModalService, TemplateModalConfig
+  , ModalTemplate, ModalSize, SuiActiveModal
+} from 'ng2-semantic-ui';
 
-import { SuiModalService, TemplateModalConfig
-  , ModalTemplate, ModalSize, SuiActiveModal } from 'ng2-semantic-ui';
-
-import {ImageCropperComponent, CropperSettings} from 'ng2-img-cropper';
+import { ImageCropperComponent, CropperSettings } from 'ng2-img-cropper';
 import { PhotoService, Ng2ImgurUploader } from "../../services/photo.service";
 
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vendor-details',
   templateUrl: './vendor-details.component.html',
   styleUrls: ['./vendor-details.component.sass'],
   providers: [
-        PhotoService,
-        Ng2ImgurUploader,
-        ModalService]
+    PhotoService,
+    Ng2ImgurUploader,
+    ModalService]
 })
 export class VendorDetailsComponent implements OnInit {
 
   @ViewChild('cropper', undefined)
-  cropper:ImageCropperComponent;
+  cropper: ImageCropperComponent;
   enabled: boolean = false;
   enableTheme: boolean = false;
-  saveImgButton:boolean = false;
+  saveImgButton: boolean = false;
   backgroundUrl: SafeResourceUrl;
   uploading: boolean;
   isOwner: boolean;
@@ -55,8 +55,8 @@ export class VendorDetailsComponent implements OnInit {
     private vendorService: VendorService,
     private modalService: ModalService,
     private photoService: PhotoService,
-    private sanitizer: DomSanitizer
-  ) {
+    private sanitizer: DomSanitizer,
+    private tokenHelperService: TokenHelperService) {
     this.getCurrentRole();
     this.cropperSettings = modalService.cropperSettings;
     this.data = {};
@@ -77,36 +77,33 @@ export class VendorDetailsComponent implements OnInit {
       this.tabActive = true;
     }
   }
-  getCurrentRole()
-  {
-    let token = localStorage.getItem('token');
-    if(token===null)
-     { 
-       this.isGuest=true;
-       return;
-     }
-    const userClaims = new JwtHelper().decodeToken(token);
-    if(userClaims['roleid']!=1)
-        this.isGuest=false; else
-    this.isGuest=true;
+
+  getCurrentRole() {
+    if (this.tokenHelperService.getToken() === null) {
+      this.isGuest = true;
+      return;
+    }
+
+    const userRoleId = +this.tokenHelperService.getClaimByName('roleid');
+    this.isGuest = userRoleId === 1;
   }
 
   buildSafeUrl(link: string): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustStyle(`url('${link}')`);
   }
-  
+
   bannerListener($event) {
-      let file: File = $event.target.files[0];
-      this.uploading = true;
-      this.photoService.uploadToImgur(file).then(link => {
-        console.log(link);
-        return this.photoService.saveBanner(link);
-      }).then(link => {
-        this.backgroundUrl = this.buildSafeUrl(link);
-        this.uploading = false;
-      }).catch(err => {
-        console.log(err);
-        this.uploading = false;
-      });
+    let file: File = $event.target.files[0];
+    this.uploading = true;
+    this.photoService.uploadToImgur(file).then(link => {
+      console.log(link);
+      return this.photoService.saveBanner(link);
+    }).then(link => {
+      this.backgroundUrl = this.buildSafeUrl(link);
+      this.uploading = false;
+    }).catch(err => {
+      console.log(err);
+      this.uploading = false;
+    });
   }
 }
