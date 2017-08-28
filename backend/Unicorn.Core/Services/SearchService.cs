55df393084dba8db2d9fd3893c19e73616f0b2c7
Ihor Horash <gorashigor@gmail.com>
2017-08-28 17:47:58 +0300

    Added search controller, service and dto

---
+++ backend/Unicorn.Core/Services/SearchService.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Unicorn.Core.Interfaces;
+using Unicorn.DataAccess.Interfaces;
+using Unicorn.Shared.DTOs.Search;
+
+namespace Unicorn.Core.Services
+{
+    public class SearchService : ISearchService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SearchService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<SearchPerformerDTO>> GetSearchPerformers()
+        {
+            var reviews = await _unitOfWork.ReviewRepository.GetAllAsync();
+
+            var vendorsList = await _unitOfWork.VendorRepository
+                .Query
+                .Include(v => v.Person)
+                .Include(v => v.Person.Account)
+                .ToListAsync();
+
+            var vendors = vendorsList
+                .Select(v => new SearchPerformerDTO
+                {
+                    Id = v.Id,
+                    Avatar = v.Person.Account.Avatar,
+                    Name = v.Person.Name,
+                    Rating = CalculateRating(v.Person.Account.Id),
+                    ReviewsCount = reviews.Count(r => r.ToAccountId == v.Person.Account.Id),
+                    PerformerType = "vendor",
+                    Link = "vendor/" + v.Id,
+                    City = v.Person.Location.City
+                }).ToList();
+
+            var companiesList = await _unitOfWork.CompanyRepository
+                .Query
+                .Include(c => c.Account)
+                .ToListAsync();
+
+            var companies = companiesList
+                .Select(c => new SearchPerformerDTO
+                {
+                    Id = c.Id,
+                    Avatar = c.Account.Avatar,
+                    Name = c.Name,
+                    Rating = CalculateRating(c.Account.Id),
+                    ReviewsCount = reviews.Count(r => r.ToAccountId == c.Account.Id),
+                    PerformerType = "company",
+                    Link = "company/" + c.Id,
+                    City = c.Location.City
+                }).ToList();
+
+            var searchPerformers = vendors
+                .Concat(companies)
+                .OrderByDescending(p => p.Rating)
+                .Distinct()
+                .ToList();
+
+            return searchPerformers;
+        }
+
+        private double CalculateRating(long recieverId)
+        {
+            var ratings = _unitOfWork.RatingRepository.Query
+                  .Include(y => y.Reciever)
+                  .Where(p => p.Reciever.Id == recieverId)
+                  .ToList();
+
+            return ratings.Any() ? ratings.Average(z => z.Grade) : 0;
+        }
+    }
+}
---
+++ backend/Unicorn.Shared/DTOs/Search/SearchPerformerDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unicorn.Shared.DTOs.Search
+{
+    public class SearchPerformerDTO
+    {
+        public long Id { get; set; }
+
+        public double Rating { get; set; }
+
+        public int ReviewsCount { get; set; }
+
+        public string Name { get; set; }
+
+        public string Avatar { get; set; }
+
+        public string PerformerType { get; set; } //vendor or company
+
+        public string Link { get; set; }
+
+        public string City { get; set; }
+    }
+}
