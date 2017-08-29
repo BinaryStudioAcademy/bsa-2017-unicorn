adb1a3c30f3908d1e5e02709a3546528783d1ac4
Nikita <dev.potapy4@hotmail.com>
2017-08-29 12:59:58 +0300

    Get all dialogs | Remove methods
    TODO: fix participant

--- backend/Unicorn.Shared/DTOs/Chat/ChatDialogDTO.cs
+++ backend/Unicorn.Shared/DTOs/Chat/ChatDialogDTO.cs
@@ -2,11 +2,17 @@
 
 namespace Unicorn.Shared.DTOs.Chat
 {
+    public class Participant
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
     public class ChatDialogDTO
     {
         public long Id { get; set; }
-        public long ParticipantOneId { get; set; }
-        public long ParticipantTwoId { get; set; }
+        public Participant ParticipantOne { get; set; }
+        public Participant ParticipantTwo { get; set; }
         public virtual ICollection<ChatMessageDTO> Messages { get; set; }
     }
 }
