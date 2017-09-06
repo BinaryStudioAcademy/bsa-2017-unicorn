ba13b5a35b146bcee1495b0dd28ec76c26d1dac2
Nikita <dev.potapy4@hotmail.com>
2017-09-06 19:25:04 +0300

    Fix for route path

--- frontend/src/app/services/helper/helper.service.ts
+++ frontend/src/app/services/helper/helper.service.ts
@@ -14,14 +14,7 @@ export class HelperService {
     const userRoleId = +userClaims['roleid'];
     const pathFrom = window.location.pathname;
 
-    /* Someone wanna break it
-    if (userRoleId === 3 || userRoleId === 4) {
-      this.router.navigate(['dashboard']);
-      return;
-    }
-    */
-
-    let path = pathFrom.includes(('/vendor')) || pathFrom.includes(('/company')) ? pathFrom : new RoleRouter().getRouteByRole(userRoleId);
+    let path = pathFrom.includes(('/vendor/')) || pathFrom.includes(('/company/')) ? pathFrom : new RoleRouter().getRouteByRole(userRoleId);
     userRoleId === 2 ? this.router.navigate([path]) : this.router.navigate([path, userClaims['profileid'], 'edit']);
   }
 }
