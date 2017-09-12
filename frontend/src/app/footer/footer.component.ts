ea34b883314c22cd225b4255fbdb9e17957bc31a
Ihor Horash <gorashigor@gmail.com>
2017-09-13 00:24:39 +0300

    Feedback feature at footer started

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-// import { MenuComponent } from '../menu/menu.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import {SuiModule} from 'ng2-semantic-ui';
+
+import { SuiModalService, TemplateModalConfig
+  , ModalTemplate, ModalSize, SuiActiveModal } from 'ng2-semantic-ui';
+
+import { ModalService } from '../services/modal/modal.service';
+
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.sass']
+  styleUrls: ['./footer.component.sass'],
+  providers: [ModalService]
 })
 export class FooterComponent implements OnInit {
+  @ViewChild('modalTemplate')
+  public modalTemplate: ModalTemplate<void, {}, void>;
+  private activeModal: SuiActiveModal<void, {}, void>;
 
-  constructor() { }
+  constructor(private modalService: ModalService) { }
 
   ngOnInit() {
   }
 
+  public openModal() {
+    this.activeModal = this.modalService.openModal(this.modalTemplate);
+  }
 }
