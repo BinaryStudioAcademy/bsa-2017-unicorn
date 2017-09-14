c7bf80d52b8bb64a25365f775c754946e7d38701
Taras Parfeniuk <taras.parfeniuk@gmail.com>
2017-09-14 14:58:07 +0300

    Base features for ban list complete

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 import { AdminComponent } from './admin/admin.component';
 import { BanListComponent } from './admin/ban-list/ban-list.component';
 import { CategoriesComponent } from './admin/categories/categories.component';
 import { AdminRoutingModule } from "./admin-routing.module";
 import { SuiModule } from "ng2-semantic-ui/dist";
+import { AccountService } from "../services/account.service";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     SuiModule,
     AdminRoutingModule
   ],
-  declarations: [AdminComponent, BanListComponent, CategoriesComponent]
+  declarations: [
+    AdminComponent,
+    BanListComponent, 
+    CategoriesComponent
+  ],
+  providers: [
+    AccountService
+  ]
 })
 export class AdminModule { }
--- frontend/src/app/admin/admin/ban-list/ban-list.component.ts
+++ frontend/src/app/admin/admin/ban-list/ban-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AdminAccountViewModel } from "../../../models/admin/admin-account-view.model";
+import { AccountService } from "../../../services/account.service";
+
 @Component({
   selector: 'app-ban-list',
   templateUrl: './ban-list.component.html',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BanListComponent implements OnInit {
 
-  constructor() { }
+  accounts: AdminAccountViewModel[];
+  page: number = 1;
+  pageSize: number = 20;
+  totalCount: number = 0;
+
+  pendingAccounts: AdminAccountViewModel[];
+
+  isLoaded: boolean;
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
+    this.load();
+    this.pendingAccounts = [];
+  }
+
+  load(): void {
+    this.isLoaded = false;
+    this.pendingAccounts = [];
+
+    this.accountService.getBanListPage(this.page, this.pageSize)
+      .then(p => {
+        this.page = p.CurrentPage;
+        this.pageSize = p.PageSize;
+        this.totalCount = p.TotalCount;
+        this.accounts = p.Items;
+
+        this.isLoaded = true;
+      })
+      .catch(err => this.isLoaded = true);
+  }
+
+  isAccountPending(account: AdminAccountViewModel): boolean {
+    return this.pendingAccounts.includes(account);
+  }
+
+  banAccount(account: AdminAccountViewModel): void {
+    this.pendingAccounts.push(account);
+
+    this.accountService.banAccount(account.Id)
+      .then(() => {
+        account.IsBanned = true;
+        this.pendingAccounts.splice(this.pendingAccounts.findIndex(a => a.Id === account.Id));
+      })
+      .catch(err => this.pendingAccounts.splice(this.pendingAccounts.findIndex(a => a.Id === account.Id)));
+  }
+
+  unbanAccount(account: AdminAccountViewModel): void {
+    this.pendingAccounts.push(account);
+
+    this.accountService.unbanAccount(account.Id)
+      .then(() => {
+        account.IsBanned = false;
+        this.pendingAccounts.splice(this.pendingAccounts.findIndex(a => a.Id === account.Id));
+      })
+      .catch(err => this.pendingAccounts.splice(this.pendingAccounts.findIndex(a => a.Id === account.Id)));
   }
 
 }
